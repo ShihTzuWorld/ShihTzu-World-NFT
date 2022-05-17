4975e9a8e0731b764537e9bde8f3c2b781c0a182
Karlitoyo <karltimmins@gmail.com>
2022-05-17 05:22:42 +0100

    Added further styling and active class to nav items

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,7 +3,7 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
-import Link from 'next/link';
+import ActiveLink from '../link';
 
 const navigation = [
     { name: 'Marketplace', href: '/', current: true },
@@ -43,20 +43,18 @@ export default function Navbar() {
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <Link
+                                            <ActiveLink
                                                 key={item.name}
                                                 href={item.href}
+                                                activeClass="bg-gray-900 text-white"
                                             >
                                                 <a
-                                                    className={classNames(
-                                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                        'px-3 py-2 rounded-md text-sm font-medium'
-                                                    )}
+                                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                                                     aria-current={item.current ? 'page' : undefined}
                                                 >
                                                     {item.name}
                                                 </a>
-                                            </Link>
+                                            </ActiveLink>
                                         ))}
                                     </div>
                                 </div>
@@ -99,7 +97,7 @@ export default function Navbar() {
 
                                                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                         >
-                                                            Your Profile
+                                                            Profile
                                                         </a>
                                                     </Link>
                                                 )}
