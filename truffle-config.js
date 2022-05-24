0802e04ceabe49a58106fbc07c470eed0640c0bf
Karlitoyo <karltimmins@gmail.com>
2022-05-24 14:52:25 +0100

    Added openzeppelin contracts

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
     development: {
-     host: "172.30.208.1",
+     host: "172.29.144.1",
      port: 7545,
      network_id: "*",
     },
