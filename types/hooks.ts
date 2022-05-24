167139ab8c4bf9b9bd2fced70ddd2e9294f4399c
Karlitoyo <karltimmins@gmail.com>
2022-05-24 18:10:55 +0100

    Added hooks types for hooks + swr dependency

---
+++ types/hooks.ts
@@ -0,0 +1,25 @@
+import { MetaMaskInpageProvider } from "@metamask/providers"
+import { Contract, providers } from "ethers"
+import { SWRResponse } from "swr";
+
+export type Web3Dependencies = {
+    provider: providers.Web3Provider,
+    contract: Contract,
+    ethereum: MetaMaskInpageProvider
+}
+
+export type CryptoHookFactory<D = any, P = any> = {
+    (d: Partial<Web3Dependencies>): CryptoSwrHandlerHook<D, P>
+}
+
+export type CryptoSwrHandlerHook<D = any, P = any> = (params: P) => CryptoSwrResponse<D>
+
+export type CryptoSwrResponse<D = any> = SWRResponse<D>;
+
+
+// export type CryptoHookFactory<D = any, P = any> = {
+//     (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>
+// }
+
+
+
