eac68de8ec59025cd6fd1a7ea05f3604cffcc193
Karlitoyo <karltimmins@gmail.com>
2022-05-24 19:55:46 +0100

    Added change of account handler to connect wallet function

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,4 +1,5 @@
 import { CryptoHookFactory } from "@_types/hooks";
+import { useEffect } from "react";
 import useSWR from "swr";
 
 type UseAccountResponse = {
@@ -23,6 +24,24 @@ export const hookFactory: AccountHookFactory = ({ provider, ethereum }) => () =>
         revalidateOnFocus: false
     }
     )
+
+    useEffect(() => {
+        ethereum?.on("accountsChanged", handleAccountsChanged);
+        return () => {
+            ethereum?.removeListener("accountsChanged", handleAccountsChanged);
+        }
+    })
+
+    const handleAccountsChanged = (...args: unknown[]) => {
+        const accounts = args[0] as string[];
+        if (accounts.length === 0) {
+            console.error("Please Connect to Web3 Wallet!");
+        } else if (accounts[0]! == swrRes.data) {
+            alert("Account has changed!");
+            console.log(accounts[0]);
+        }
+    }
+
     const connect = async () => {
         try {
             ethereum?.request({ method: "eth_requestAccounts" });
