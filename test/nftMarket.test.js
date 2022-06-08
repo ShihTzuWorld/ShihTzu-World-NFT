80e0e146373677546e7475c13b87d8b9dd1b20a4
Karlitoyo <karltimmins@gmail.com>
2022-06-08 14:50:39 +0100

    Added listed item count + price for nft mint

--- test/nftMarket.test.js
+++ test/nftMarket.test.js
@@ -1,8 +1,10 @@
 const NftMarket = artifacts.require("NftMarket");
 
 contract("NftMarket", accounts => {
+const {ethers} = require("ethers");
 
 let _contract = null;
+let _nftPrice = ethers.utils.parseEther("0.3").toString();
 
 before(async () => {
     _contract = await NftMarket.deployed();
@@ -11,7 +13,7 @@ before(async () => {
 describe("Mint token", () => {
     const tokenURI = "https://test.com";
     before(async () => {
-        await _contract.mintToken(tokenURI, {
+        await _contract.mintToken(tokenURI, _nftPrice, {
             from: accounts[0]
         })
     })
@@ -28,7 +30,7 @@ describe("Mint token", () => {
 
     it("Should not be possible to create NFT with used token URI", async () => {
         try {
-            await _contract.mintToken(tokenURI, {
+            await _contract.mintToken(tokenURI, _nftPrice, {
                 from: accounts[0]
             })
             
@@ -36,7 +38,11 @@ describe("Mint token", () => {
              assert(error, "NFT minted with previously used token URI");
         }
 
-       
+    })
+
+    it("Should have 1 listed item", async () => {
+        const listedItemCount = await _contract.listedItemsCount();
+        assert.equal(listedItemCount, 1, "Listed items count is not 1");
     })
 })
 
