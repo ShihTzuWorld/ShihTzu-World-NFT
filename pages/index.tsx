47b5f562b225f866d4537749af03f6da52cc1724
Karlitoyo <karltimmins@gmail.com>
2023-03-24 01:34:05 +0000

    added stock shih to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,32 @@
-import { BaseLayout, Navbar, NftList } from '@ui';
-import Image from 'next/image';
-import Shihverse from '../public/images/shih-tzu.png';
-import Link from 'next/link';
-import { ExclamationIcon } from '@heroicons/react/solid';
+/* eslint-disable @next/next/no-img-element */
+
+import type { NextPage } from 'next';
+import { BaseLayout, NftList } from '@ui';
 import { useNetwork } from '@hooks/web3';
+import { ExclamationIcon } from '@heroicons/react/solid';
 
-
-// const myLoader = ({ src }) => {
-//   return `/preprocessed-images/${src}`;
-// };
-
-const IndexPage = () => {
+const Home: NextPage = () => {
     const { network } = useNetwork();
-    return (
-        <div className="bg-gradient-to-tr from-red-500 to-purple-400 relative h-screen overflow-hidden">
-            <div className='md:flex md:justify-center lg:justify-left'>
-                <div>
-                    <Image src={Shihverse}
-                        // loader={myLoader}
-                        className="inset-0 w-full md:justify-center h-full object-cover filter brightness-50"
-                        alt="main background image"
-                    ></Image>
-                </div>
-                <div className="lg:relaive md:relative inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto lg:mr-60 text-center overflow-hidden">
 
-                    <h1 className="font-primary font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-snug">
-                        SHIHverse NFT
-                    </h1>
-                    <div className="space-y-8 ">
-                        <h1 className="font-primary font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-tight">
-                            Easily Mint NFT'S on {' '}
-                            <span className="text-palette-primary">
-                                SHIH.
-                            </span>
-                        </h1>
-                        <p className="font-secondary text-palette-light text-base md:text-lg lg:text-xl">
-                            All your NFT's in one Doggy dogg world!
+    return (
+        <BaseLayout>
+            <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+                <div className="absolute inset-0">
+                    <div className="bg-white h-1/3 sm:h-2/3" />
+                </div>
+                <div className="text-center home-box">
+                    <div className="relative">
+                        <div>
+                            <img
+                                className="inline-block h-44 w-44 card-nft-home"
+                                src="/images/header-picture-landing.png"
+                                alt=""
+                            />
+                        </div>
+                        <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">SHIHverse NFTs</h2>
+                        <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+                            All your NFT's in one doggy dog world!
                         </p>
-                        <Link href="/marketplace">
-                            <button type="button" className="py-4 px-6 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 w-9/12 rounded-full">
-                                Enter App
-                            </button>
-                        </Link>
                     </div>
                     {network.isConnectedToNetwork ?
                         <NftList /> :
@@ -64,11 +49,10 @@ const IndexPage = () => {
                             </div>
                         </div>
                     }
-
                 </div>
             </div>
-        </div>
+        </BaseLayout>
     )
 }
 
-export default IndexPage
+export default Home
